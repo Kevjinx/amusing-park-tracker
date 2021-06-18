7885bb006b18d50ef6bebc7efe197e0545056851
kevjinx <kevin415mei@gmail.com>
2021-06-18 15:33:46 -0700

    add custom error handling

--- app.js
+++ app.js
@@ -1,18 +1,55 @@
 const express = require('express')
 const app = express()
 const routes = require('./routes')
-const port = 8080
 const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+require('dotenv').config();
+
+
 
 app.set('view engine', 'pug')
 app.use(morgan('dev'))
+app.use(express.urlencoded({ extended: false }))
 
 app.use(routes)
+app.use(cookieParser())
+
+const pageNotFoundError = (req, res, next) => {
+    let testError = new Error("The requests page couldn't be found");
+    testError.status = 404
+    console.log(res);
+    next(testError)
+}   
+
+const errorHandler = (err, req, res, next) => {
+    if (err.status === 404) {
+        res.status(404)
+        res.render('page-not-found')
+    } else if (err.status === 500) {
+        res.status(500)
+        res.render('server-error', {
+            message: err.message,
+            stack: err.stack
+        })
+    } else {
+        console.error(err);
+        console.error(err.stack);
+        next(err)
+    }
+}
+
+
+app.get('/', (req, res) => {
+    res.render('layout')
+})
 
 
 
 
-
-app.listen(port, console.log(`listening to port ${port}`))
+app.use(pageNotFoundError)
+app.use(errorHandler)
 
 module.exports = app
+
+
+
